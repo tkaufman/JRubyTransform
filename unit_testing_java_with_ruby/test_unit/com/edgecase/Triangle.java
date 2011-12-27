f9cc3c6dd6cb0b274a371ca4f497a27046c2b475
Joe O'Brien <joe@edgecase.com>
2011-12-27 16:08:18 -0500

    first passing triangle_test

---
+++ unit_testing_java_with_ruby/test_unit/com/edgecase/Triangle.java
@@ -0,0 +1,24 @@
+package com.edgecase;
+
+public class Triangle {
+	int a;
+	int b;
+	int c;
+
+	public Triangle(int _a, int _b, int _c) {
+		a = _a;
+	    b = _b;
+		c = _c;
+	}
+	
+	public String classify() {
+		if ((a == b) && (b == c)) {
+			return "equilateral";
+		}
+		if ((a == b) || (b == c) || (a == c)) {
+			return "isosceles";
+		}
+		return "scalene";
+	}
+
+}
